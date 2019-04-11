3837b22abee1e3c73954a9cbc0d12e6c3f7f7bb1
Fred <fred.paucarespinoza@gmail.com>
2019-04-10 19:01:34 -0500

    dec app

--- src/app.js
+++ src/app.js
@@ -1,7 +1,25 @@
+// para evitar el problemas de concatenacion de rutas
+const path = require('path');
+const morgan = require('morgan');
 const express = require('express');
-
 const app = express();
 
-app.listen(3000, () => {
-    console.log('Server on port 3000');
-})
+// importing routes
+const indexRoutes = require('./routes/index');
+
+// settings
+app.set('port', process.env.PORT || 8000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view.engine', 'ejs');
+
+// middlewares
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: false }));
+
+// routes
+app.use('/', indexRoutes);
+
+// starting server
+app.listen(app.get('port'), () => {
+    console.log(`Server on port ${app.get('port')}`);
+});
